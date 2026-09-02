08344b9cf00513f95251d4ab0d0c1f98dc7e732d
KolKemboi <kolkemboi@gmail.com>
2026-09-02 16:48:04 +0300

    Lexxing

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,32 @@
 #include "headers/Lexer.hpp"
 #include <cstdio>
-int main() { printf("Hello World, Kompiler\n"); }
+#include <string>
+#include <vector>
+
+/////LANGUAGE STRUCTURE
+/*
+ * int main {
+ * int int_name = 0;
+ * bool bool_name = true;
+ * str str_name = "string";
+ * return 1;
+ * }
+ *
+ */
+
+int main(int argc, char *argv[]) {
+  std::string_view SourceCode =
+      "int main { int int_name = 0; bul bool_name = "
+      "true; flt float_name = 1.009; str str_name = \"string\"; ret 1;}";
+  printf("Source code \n\t %s\n", std::string(SourceCode).c_str());
+  Lexer lexer = Lexer(SourceCode);
+  std::vector<Token> tokens = lexer.tokenize();
+
+  for (Token &tok : tokens) {
+    if (tok.s_Type == TokenType::END_OF_FILE) {
+      printf("EOF found, line %zu\n", tok.s_Line);
+    }
+    if (tok.s_Type == TokenType::IDENTIFIER) {
+      printf("%s\n", std::string(tok.s_Value).c_str());
+  }
+}
